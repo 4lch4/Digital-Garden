ce5db1f7bbbd5b2622e5379a0124d59669966186
4lch4 <git@4lch4.dev>
2021-05-09 07:07:15 -0500

    feat: updated references to my info

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,82 +1,69 @@
+const OrgName = '4lch4'
+const ProjectName = 'Knowledge-Base'
+
 module.exports = {
-  title: 'My Site',
-  tagline: 'The tagline of my site',
-  url: 'https://your-docusaurus-test-site.com',
+  title: `4lch4 - Knowledge Base`,
+  tagline:
+    'My collection of "shared knowledge", which at the moment mostly consists of TIL snippets.',
+  url: 'https://kba.4lch4.dev',
   baseUrl: '/',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: OrgName, // Usually your GitHub org/user name.
+  projectName: ProjectName, // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'My Site',
+      title: `@4lch4/knowledge-base`,
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        alt: "4lch4's Knowledge Base Logo",
+        src: 'img/prime-logo.svg'
       },
-      links: [
+      items: [
         {
           to: 'docs/doc1',
-          activeBasePath: 'docs',
-          label: 'Docs',
-          position: 'left',
+          activeBasePath: 'tils',
+          label: 'TILs',
+          position: 'left'
         },
-        {to: 'blog', label: 'Blog', position: 'left'},
+        { to: 'blog', label: 'Blog', position: 'left' },
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/4lch4/TILs',
           label: 'GitHub',
-          position: 'right',
-        },
-      ],
+          position: 'right'
+        }
+      ]
     },
     footer: {
       style: 'dark',
       links: [
         {
-          title: 'Docs',
+          title: 'TILs',
           items: [
             {
               label: 'Style Guide',
-              to: 'docs/doc1',
+              to: 'docs/doc1'
             },
             {
               label: 'Second Doc',
-              to: 'docs/doc2',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
+              to: 'docs/doc2'
+            }
+          ]
         },
         {
           title: 'More',
           items: [
             {
               label: 'Blog',
-              to: 'blog',
+              to: 'blog'
             },
             {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
-        },
+              href: 'https://github.com/4lch4/TILs'
+            }
+          ]
+        }
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-    },
+      copyright: `Copyright © ${new Date().getFullYear()} 4lch4 Industries, LLC. Built with Docusaurus.`
+    }
   },
   presets: [
     [
@@ -86,18 +73,18 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/4lch4/knowledge-base/edit/master/website/'
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/4lch4/knowledge-base/edit/master/website/blog/'
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      },
-    ],
-  ],
-};
+          customCss: require.resolve('./src/css/custom.css')
+        }
+      }
+    ]
+  ]
+}
