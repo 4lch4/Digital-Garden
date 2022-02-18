791ff7b4e0342986f10b355a55ab87a60b8fbaf0
4lch4 <git@4lch4.email>
2022-02-17 21:27:35 -0600

    feat: moved countly support to another page

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,27 @@ function Home() {
     <Layout description={siteConfig.tagline}>
       <Head>
         <script type="text/javascript" src="https://js-cdn.dynatrace.com/jstag/16362cc0dec/bf48922oxv/4b6329f719b105fd_complete.js" crossorigin="anonymous"></script>
+        <script type="text/javascript" src="https://countly.4lch4.cloud/sdk/web/countly.min.js"></script>
+        <script type="text/javascript">
+          //some default pre init
+          var Countly = Countly || {};
+          Countly.q = Countly.q || [];
+
+          //provide countly initialization parameters
+          Countly.app_key = '80ac8638fb55fb5110b76eb4d7892db98e514035';
+          Countly.url = 'https://countly.4lch4.cloud';
+
+          Countly.q.push(['track_sessions']);
+          Countly.q.push(['track_pageview']);
+          Countly.q.push(['track_clicks']);
+          Countly.q.push(['track_scrolls']);
+          Countly.q.push(['track_errors']);
+          Countly.q.push(['track_links']);
+          Countly.q.push(['track_forms']);
+          Countly.q.push(['collect_from_forms']);
+        </script>
       </Head>
+
       <header className={classnames('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
