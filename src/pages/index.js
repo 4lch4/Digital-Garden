02ca06fdb94a42b447221b91a20d270bce7408ae
4lch4 <git@4lch4.email>
2022-02-26 05:59:25 -0600

    feat: added ackee tracker script

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,7 @@ import Head from '@docusaurus/Head'
 import features from '../components/Features'
 import Link from '@docusaurus/Link'
 
+
 function Home() {
   const context = useDocusaurusContext()
   const { siteConfig = {} } = context
@@ -15,8 +16,7 @@ function Home() {
   return (
     <Layout description={siteConfig.tagline}>
       <Head>
-        <script type="text/javascript" src="https://js-cdn.dynatrace.com/jstag/16362cc0dec/bf48922oxv/4b6329f719b105fd_complete.js" crossorigin="anonymous"></script>
-        <script type="text/javascript" src="https://countly.4lch4.cloud/sdk/web/countly.min.js"></script>
+        <script async src="https://ackee.4lch4.cloud/tracker.js" data-ackee-server="https://ackee.4lch4.cloud" data-ackee-domain-id="b8bd5e0d-20ac-458e-9b30-7ea2d23fd1fb"></script>
       </Head>
 
       <header className={classnames('hero hero--primary', styles.heroBanner)}>
